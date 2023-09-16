164d0e899ad0bdc1316efebf356b890ab4222fc9
Pratheesh <pratheesh1218@gmail.com>
2023-09-16 18:48:49 +0530

    changes made

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes/Authentication');
-const bootsRoutes = require('./routes/BootsRoutes');
-const cartsRoutes = require('./routes/CartsRoutes');
 require('dotenv').config();
 
 
