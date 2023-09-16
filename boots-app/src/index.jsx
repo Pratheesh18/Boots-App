1db4d134e69d7ee9659880c82764fa2b5c6e56b1
Pratheesh <pratheesh1218@gmail.com>
2023-09-16 11:16:55 +0530

    added some redux configs

--- boots-app/src/index.jsx
+++ boots-app/src/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {Provider} from 'react-redux';
+import { configureStore} from '@reduxjs/toolkit';
+import rootReducer from "./reducers/RootReducer";
+import thunk from  'redux-thunk';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = configureStore({
+  reducer : rootReducer,
+  middleware : [thunk],
+})
+
 root.render(
+  <Provider store={store}>
    <Router>
     <App />
   </Router>
+  </Provider>
 );
---
+++ boots-app/src/reducers/RootReducer.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+import cartReducer from './CartReducer';
+
+
+const rootReducer = combineReducers({
+    cart : cartReducer,
+});
+
+
+export default rootReducer;
