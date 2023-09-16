5b1046ac85ee02e53d7a91f83632c7d7699d9e33
Pratheesh <pratheesh1218@gmail.com>
2023-09-16 18:48:13 +0530

    deleted some files

--- Backend/app.js
+++ Backend/app.js
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth' , routes);
-app.use('/boots' , bootsRoutes);
-app.use('/cart',cartsRoutes);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
--- Backend/routes/BootsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bootsController = require('../controllers/BootsController');
-
-router.get('/',bootsController.getAllBoots);
-
-module.exports = router;
